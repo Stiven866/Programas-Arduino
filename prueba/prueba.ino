195a42c2d305950dcd780a6f6306ecd8b0142707
Stiven Duque <sduque866@gmail.com>
2019-04-13 09:13:25 -0500

    Programa que realiza operaciones basicas

---
+++ prueba/prueba.ino
@@ -0,0 +1,25 @@
+char tecla = ' ';
+float a = 10.7, b = 5, resultado = 0;
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    tecla = Serial.read();
+    if(tecla == 'a'){
+      resultado = a + b;
+    }else if(tecla == 'b'){
+      resultado = a-b;
+    }else if(tecla == 'c'){
+      resultado = a*b;
+    }else if(tecla == 'd'){
+      resultado = a/b;
+    }
+
+    Serial.print("El resultado es: ");
+    Serial.println(resultado);
+  }
+  
+}
